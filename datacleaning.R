cb1adac69b5c24a63c1c37227d294178b822d6ff
Amin Adibi <adibi@alumni.ubc.ca>
2018-05-25 19:03:12 -0700

    tidycost now function

--- datacleaning.R
+++ datacleaning.R
@@ -1,19 +1,15 @@
 library(XLConnect)
 library(tidyverse)
 
-tidyCost <- function (excelName, province) {
+tidyCost <- function (province, type) {
   wb <- loadWorkbook("./burden_of_COPD/Burden_of_COPD_BC_ProvidenceAPR04.xlsx", create=F)
-  data <- readNamedRegion(wb, name = "excelName")
+  excelName <- paste0(province, "_cost_", type)
+  data <- readNamedRegion(wb, name = excelName)
   data$province <- province
+  data$type <- type
   data %>% select (-total) %>% gather("genderage", "value", 2:9) %>% separate (genderage, into=c("gender", "age"))
 }
 
-wb <- loadWorkbook("./burden_of_COPD/Burden_of_COPD_BC_ProvidenceAPR04.xlsx", create=F)
-BC_cost_hosp <- readNamedRegion(wb, name = "BC_cost_hosp")
-
-data <- BC_cost_hosp
-
-data$province <- 'bc'
-
-
-data %>% select (-total) %>% gather("genderage", "value", 2:9) %>% separate (genderage, into=c("gender", "age"))
+#BC_cost_hosp
+cost <- tidyCost("BC", "hosp")
+cost <- rbind(cost, tidyCost("ON", "hosp"))
