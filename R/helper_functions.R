b3761ac7c474cf2437d62d6d95c810b7152b92c8
Ainsleigh Hill <ainsleighhill@ubc.ca>
2018-10-25 14:18:20 -0700

    Moved zoomControl in leaflet so that selectInput is in front, changed leaflet legend and labels

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,20 +1,29 @@
 
-costToMill <- function(cost_vector){
+costToMill <- function(cost_vector, char = TRUE){
   figures <- c()
   for(i in 1:length(cost_vector)){
     figure <- cost_vector[i]/1000000
-    if(figure>10){
+    if(figure>=1000){
       figure <- cost_vector[i]/1000000000
-      figure <- paste0("$", as.character(round(figure, 2)), " Billion")
+      figure <- round(figure,2)
+      figure <- formatC(figure, format = 'f', digits = 1)
+      if(char){
+        figure <- paste0(as.character(figure), " Billion")
+      }
     } else if (cost_vector[i]==0) {
       figure <- "No Data"
     } else{
-      figure <- paste0("$", as.character(round(figure, 2)), " Million")
+      figure <- round(figure, 2)
+      figure <- formatC(figure, format = 'f', digits = 1)
+      if(char){
+        figure <- paste0(as.character(figure), " Million")
+      }
     }
     figures <- c(figures, figure)
   }
   return(figures)
 }
+
 
 provinceConvert <- function(provinces, to, quebec=1){
   short <- c("AB", "BC", "SK", "MB", "ON", "QC", "NL", "NT", "NU", "PE", "YT", "NS", "NB", "QC")
@@ -60,6 +69,47 @@ customLegend <- function(x){
   return(0)
 }
 
+myLabFormat <- function(
+  prefix = "", suffix = "", between = " &ndash; ", digits = 3, big.mark = ",",
+  transform = identity
+) {
+  
+  formatNum <- function(x) {
+    x <- round(transform(x), digits)
+   
+    if(max(x)>1000000){
+      x <- costToMill(x, char=TRUE)
+    }
+
+    return(x)
+  }
+  
+  function(type, ...) {
+    switch(
+      type,
+      numeric = (function(cuts) {
+        paste0(prefix, formatNum(cuts), suffix)
+      })(...), # nolint
+      bin = (function(cuts) {
+        n <- length(cuts)
+        paste0(prefix, formatNum(cuts[-n]), between, formatNum(cuts[-1]), suffix)
+      })(...), # nolint
+      quantile = (function(cuts, p) {
+        n <- length(cuts)
+        p <- paste0(round(p * 100), "%")
+        cuts <- paste0(formatNum(cuts[-n]), between, formatNum(cuts[-1]))
+        # mouse over the legend labels to see the values (quantiles)
+        paste0(
+          "<span title=\"", cuts, "\">", prefix, p[-n], between, p[-1], suffix,
+          "</span>"
+        )
+      })(...), # nolint
+      factor = (function(cuts) {
+        paste0(prefix, as.character(transform(cuts)), suffix)
+      })(...) # nolint
+    )
+  }}
+  
 
 
 
